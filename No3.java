61925626c6b875937823c0de15b45f3259e55177
Wangeldona <wangelthokar@gmail.com>
2025-06-14 21:20:33 +0545

    No3.java

--- No3.java
+++ No3.java
@@ -1,3 +1,35 @@
-public class No3 {
+abstract class Vehicle {
+    abstract void startEngine();
+    abstract void stopEngine();
+}
 
+class Car extends Vehicle {
+    void startEngine() {
+        System.out.println("Car engine started.");
+    }
+
+    void stopEngine() {
+        System.out.println("Car engine stopped.");
+    }
+}
+
+class Motorcycle extends Vehicle {
+    void startEngine() {
+        System.out.println("Motorcycle engine started.");
+    }
+
+    void stopEngine() {
+        System.out.println("Motorcycle engine stopped.");
+    }
+}
+
+public class No3 {
+    public static void main(String[] args) {
+        Vehicle car = new Car();
+        Vehicle bike = new Motorcycle();
+        car.startEngine();
+        car.stopEngine();
+        bike.startEngine();
+        bike.stopEngine();
+    }
 }
