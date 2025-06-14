6e40d9a357bff2dd432115727dc31eb98eaf3f34
Wangeldona <wangelthokar@gmail.com>
2025-06-14 21:24:21 +0545

    Update No7.java

--- No7.java
+++ No7.java
@@ -1,3 +1,36 @@
-public class No7 {
+interface Employee {
+    void work();
+    double getSalary();
+}
 
+class Chef implements Employee {
+    public void work() {
+        System.out.println("Chef is cooking.");
+    }
+
+    public double getSalary() {
+        return 30000.00;
+    }
+}
+
+class Waiter implements Employee {
+    public void work() {
+        System.out.println("Waiter is serving customers.");
+    }
+
+    public double getSalary() {
+        return 15000.00;
+    }
+}
+
+public class No7 {
+    public static void main(String[] args) {
+        Employee chef = new Chef();
+        Employee waiter = new Waiter();
+
+        chef.work();
+        System.out.println("Chef salary: " + chef.getSalary());
+        waiter.work();
+        System.out.println("Waiter salary: " + waiter.getSalary());
+    }
 }
